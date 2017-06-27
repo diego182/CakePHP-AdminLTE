a36e1bf266413f9cc225aa55baaebd2648f403c9
Diego Mendonça <diego182@gmail.com>
2017-06-27 14:34:43 -0300

    Correção do script de upload de imagens

--- webroot/js/custom.js
+++ webroot/js/custom.js
@@ -27,8 +27,8 @@ $(document).ready(function () {
 
         $this = $(this);
 
-        $.post($this.attr('href'), {'_csrfToken': readCookie('csrfToken')}, function ( data ) {
-            if('active' === data.active.status) {
+        $.post($this.attr('href'), {'_csrfToken': readCookie('csrfToken')}, function (data) {
+            if ('active' === data.active.status) {
                 $this.children(':first').removeClass('fa-square-o').addClass('fa-check-square-o');
             } else if ('deactivated' === data.active.status) {
                 $this.children(':first').removeClass('fa-check-square-o').addClass('fa-square-o');
@@ -39,28 +39,26 @@ $(document).ready(function () {
     });
 
     function showFile($data) {
-        var $filePlaceholder = $('[show-files]');
+        var $filePlaceholder = $('[data-file-placeholder]');
 
         $filePlaceholder.append('<div class="img-container" id="sort_' + $data.file.payload.id + '">');
 
         $file = $('#sort_' + $data.file.payload.id);
 
-        if($filePlaceholder.hasClass('public-site')) {
+        if ($filePlaceholder.hasClass('public-site')) {
             $file.append('<img src="' + ($data.file.url).replace('/web/', '/sq/') + '" width="150" height="150" class="img-responsive" alt="' + $data.file.payload.original_name + '">');
-        } else if($filePlaceholder.hasClass('web')) {
+        } else if ($filePlaceholder.hasClass('web')) {
             $file.append('<img src="' + ($data.file.url).replace('/original/', '/web/') + '" class="img-responsive" alt="' + $data.file.payload.original_name + '">');
-        } else if($filePlaceholder.hasClass('banner')) {
+        } else if ($filePlaceholder.hasClass('banner')) {
             $file.append('<img src="' + ($data.file.url).replace('/original/', '/1920x750/') + '" class="img-responsive" alt="' + $data.file.payload.original_name + '">');
         } else {
             $file.append('<img src="' + ($data.file.url).replace('/web/', '/sq/') + '" class="img-responsive" alt="' + $data.file.payload.original_name + '">');
         }
-        $file.append('<a href="#" class="btn btn-danger" data-remove-image-from-list>Apagar</a>');
-        $file.append('<a href="/admin/files/files/featuredToggle/' + $data.file.payload.id + '" class="text-black" data-activate-toggle><i class="fa fa-lg fa-square-o" aria-hidden="true"></i></a>');
-        $file.append('<span class="sort"><i class="fa fa-grip fa-rotate-90" data-sort-drag-area></i></span>');
-        $file.append('<input type="hidden" name="files[_ids][]" value="' + $data.file.payload.id + '">');
+
+        $file.append('<input type="hidden" name="file_id" value="' + $data.file.payload.id + '">');
     }
 
-    var $uploadFile = $('[data-upload-file]');
+    var $uploadFile = $('[data-file-upload]');
 
     /**
      * Uploads a file.
@@ -87,13 +85,10 @@ $(document).ready(function () {
             beforeSend: function () {
                 $this.siblings('i').removeClass('fa-image').addClass('fa-spinner fa-spin');
             }
-        }).done(function (data) {
-            if('undefined' !== typeof data.file.success && data.file.success) {
-
+        }).done(function (data, textStatus, jqXHR) {
+            if ('undefined' !== typeof data.file.success && data.file.success) {
+                $('.img-container').remove();
                 showFile(data);
-
-                imageWidth();
-
             } else {
                 alert('Algo errado aconteceu no envio da imagem');
             }
@@ -115,7 +110,7 @@ $(document).ready(function () {
         placeholder: 'ui-state-highlight',
         handle: '.sort',
         stop: function (event, ui) {
-            var data = $(this).sortable('serialize', { expression: '(.+)_(.+)' });
+            var data = $(this).sortable('serialize', {expression: '(.+)_(.+)'});
             $.ajax({
                 data: data,
                 type: 'POST',
@@ -130,14 +125,14 @@ $(document).ready(function () {
 
     $loadImageFromDB = $('[data-load-image-from-db]');
 
-    if($loadImageFromDB.length) {
+    if ($loadImageFromDB.length) {
 
         $.each($loadImageFromDB, function ($key, $input) {
-            $.getJSON('/en/api/file-link/' +  $($input).val(), null, function (data) {
+            $.getJSON('/en/api/file-link/' + $($input).val(), null, function (data) {
                 $($input)
                     .parent()
                     .append('' +
-                        '<img src="' + (data.file.url).replace('/original/', '/' +  $($input).data('load-image-from-db') + '/') + '" class="img-responsive" alt="' + data.file.payload.original_name + '">'
+                        '<img src="' + (data.file.url).replace('/original/', '/' + $($input).data('load-image-from-db') + '/') + '" class="img-responsive" alt="' + data.file.payload.original_name + '">'
                     );
                 $($input)
                     .siblings('div.loader')
