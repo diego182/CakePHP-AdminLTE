aa7477cbbd2de251badd9916be554964767371bb
Diego Andrade de Mendonça <diego182@gmail.com>
2018-01-11 16:23:56 -0200

    Ferramentas para o build de frontend

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,20 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
-    // return console.log('Task ran');
-});
-
-gulp.task('site', function() {
+gulp.task('site', function () {
     return gulp.src([
-        './assets/js/html5shiv.min.js',
-        './assets/js/css3-mediaqueries.js',
-        './assets/js/jquery-3.1.1.js',
-        './assets/js/parallax.js',
-        './assets/js/smoothscroll.js',
-        './assets/js/jquery.tooltipster.min.js',
-        './assets/js/jquery.rwdImageMaps.js',
-        './assets/js/jquery.bxslider.js',
-        './assets/js/jquery.flexslider.js',
-        './assets/js/cta.js',
-        './assets/js/script-modal-nav.js',
-        './assets/js/scripts.js'
+        './assets/js/jQuery.js',
+        './assets/js/jquery-ui.js',
+        './assets/js/bootstrap.js',
+        './assets/js/jquery.slimscroll.js',
+        './assets/js/fastclick.js',
+        './assets/js/adminlte.js',
+        './assets/js/fontawesome-all.js',
+        './assets/js/custom.js',
+        './assets/js/upload.js',
+        './assets/js/toggle.js'
     ])
         .pipe(concat('script.min.js'))
-        // .pipe(uglify())
-        .pipe(gulp.dest('./pt/webroot/js'))
-        .pipe(gulp.dest('./en/webroot/js'));
+        .pipe(uglify())
+        .pipe(gulp.dest('./webroot/js'));
 });
